92b0ecf432640a4beb46a46cc543bff297f7230e
vlada1001 <vlada.pavlovic0@gmail.com>
2020-03-16 10:40:55 +0100

    uradio a, b, c

--- QualificationRound/p2_WisePersonInTheMountain.py
+++ QualificationRound/p2_WisePersonInTheMountain.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import os, fnmatch, re
+import os
+import re
 
 # Paths
 root = r'/home/littlewing/Projects/MachineLearningMicrosoftPetnica/QualificationRound/p2_publicDataSet/'
@@ -10,8 +11,8 @@ data_set = r'/home/littlewing/Projects/MachineLearningMicrosoftPetnica/Qualifica
 
 questions = {'Yes': 0, 'No': 0, 'Valid': 0, 'Total': 0}
 threshold = 0.7
-TPR, FPR = 0, 0
-EER = 0
+TPR, FPR = 0.0, 0.0
+EER = 0.0
 
 data_set_folder = str(input())
 curr_data_set = data_set + data_set_folder
@@ -20,27 +21,42 @@ regex = 'ca(\d+)\.txt|wpa(\d+)\.txt'
 re_obj = re.compile(regex)
 
 mapa_ca = dict()
-mapa_wap = dict()
+mapa_wpa = dict()
 
-# get ca and wap file content
+mapa_valid = []
+
+# Get ca and wpa file content
 for root_, dir_, files in os.walk(curr_data_set):
     for item in files:
-        # get file path
+        # Get file path
         item_path = root_ + '/' + item
 
-        # enter file
+        # Enter file
         item_content = ''
         with open(item_path, 'r') as f:
-            # read file content
+            # Read file content
             item_content = f.readline().strip('% \n')
 
         matches = re_obj.match(item)
         if matches.group(1) == None:
-            mapa_wap[matches.group(2)] = float(item_content) / 100
+            perc = float(item_content) / 100
+            mapa_wpa[matches.group(2)] = perc
         else:
             mapa_ca[matches.group(1)] = str(item_content)
             questions['Total'] += 1
             questions[item_content] += 1
 
+# Get valid questions
+for k in mapa_ca:
+    if k in mapa_wpa:
+        mapa_valid.append(k)
+        questions['Valid'] += 1
+
+for el in mapa_valid:
+    pass
+    # print("{:>3} : {:>3} : {:>3}".format(el, mapa_ca[el], mapa_wpa[el]))
+
+pq, nq, vq, tq = questions.values()
+
+print('{},{},{},{},{},{}'.format(pq, nq, vq, TPR, FPR, EER))
 print(questions)
-                
